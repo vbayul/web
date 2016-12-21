7863085206ada99502ddca3d26443f735c68066c
Владимир <vbayul@gmail.com>
2016-12-21 12:50:48 +0200

    Small changes

--- src/main/java/org/luxoft/hibernate/HibernateMapping.java
+++ src/main/java/org/luxoft/hibernate/HibernateMapping.java
@@ -8,25 +8,19 @@ import org.luxoft.object.Line;
 
 public class HibernateMapping {
 
-	// метод для получения списка массива файлов
 	public List<File> files()
 	{
-		// получаем сессию
 	    Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-	    // начинаем транзакцию
 		session.beginTransaction();
 		@SuppressWarnings("unchecked")
 		List<File> result = session.createQuery("from File").list();
-		session.getTransaction().commit();// подтвержение транзакции
+		session.getTransaction().commit();
 		return result;	
 	}
 	
-	// метод для получения массима встрок из базы для конкретного файла
 	public List<Line> lines(int id)
 	{
-		// получаем сессию для подключения
 	    Session session = HibernateUtil.getSessionFactory().getCurrentSession();
-	    // начинаем транзакцию
 		session.beginTransaction();
 		@SuppressWarnings("unchecked")
 		List<Line> result = session.createCriteria(Line.class).add(Restrictions.eq("idfile", id)).list();
